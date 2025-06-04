305912b33e0dc8a0fd9022e19125b2f62205e2fa
ansh <ianshpwr@gmail.com>
2025-06-05 02:34:18 +0530

    added features in landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 import Navbar from '@/Components/Navbar';
 import Landingpage from '@/Components/Landingpage/Landingpage';
+import FeaturesSection from '@/Components/ui/Bentogrid';
+
 export default function Home() {
   return (
     <>
     <div>
       <Landingpage />
+      <FeaturesSection />
     </div>
     </>
   );
