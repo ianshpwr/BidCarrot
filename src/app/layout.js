501d219ff2628a795d3540b8acc1133f7959424b
ansh <ianshpwr@gmail.com>
2025-06-05 10:49:32 +0530

    added auction icon in the title bar

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,16 +4,18 @@ import Navbarr from '@/Components/Navbar';
 export const metadata = {
   title: "BidCarrot",
   description: "Auction Platform",
+  icons: {
+    icon: "/auction.png",
+  },
 };
-
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <div className="originalNavbar">
-            <Navbarr />
-            </div>
+          <Navbarr />
+        </div>
         {children}
       </body>
     </html>
