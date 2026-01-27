4762ade202b78288564910df5cd3b61e52ac418e
ansh <ianshpwr@gmail.com>
2026-01-27 15:03:07 +0530

    thak gaya hu m

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,5 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    // This forces the variable to be exposed if it exists
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
   images: {
     remotePatterns: [
       {
