d13d2926b50d4cc558df509e677b3a08fc87c757
ansh <ianshpwr@gmail.com>
2026-01-27 10:54:08 +0530

    CHANGED LOGIN TO POST

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const authController = require("../controllers/auth.controller.js");
 
 router.post("/signup", authController.signup);
-router.get("/login", authController.login);
+router.post("/login", authController.login);
 
 module.exports = router;
